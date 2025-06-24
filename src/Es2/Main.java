54ed4aff3528adbd6bad668619c755896f584fb1
ZairaStra <straticozaira@gmail.com>
2025-06-24 16:59:24 +0200

    3?

--- src/Es2/Main.java
+++ src/Es2/Main.java
@@ -1,4 +1,49 @@
 package Es2;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rndm = new Random();
+
+        System.out.println("INserisci il numero di valori da generare");
+        int N = Integer.parseInt(scanner.nextLine());
+
+        //devo usare Integer, non accetta int
+        //ArrayList<Integer> nums = new ArrayList<>();
+        //uso upcasting
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            nums.add(rndm.nextInt(101));
+        }
+
+        Collections.sort(nums);
+        System.out.println("La lista ordinata è: " + nums);
+
+        //creo una seconda lista con gli elementi della prima a cu iaggiungere poi la lista invertita
+        List<Integer> secondNums = new ArrayList<>(nums);
+        List<Integer> toInvert = new ArrayList<>(nums);
+
+        Collections.reverse(toInvert);
+        secondNums.addAll(toInvert);
+
+        System.out.println("La lista a specchio è: " + secondNums);
+
+        //determino la stampa di pari o dispari
+        System.out.println("Digita true per stampare i numeri pari della lista, false per quelli dispari");
+        //true=evenNums, false=!evenNums
+        boolean evenNums = Boolean.parseBoolean(scanner.nextLine());
+
+        for (int i = 0; i < secondNums.size(); i++) {
+            if (i % 2 == 0 && evenNums) {
+                System.out.println(secondNums.get(i));
+            } else if (i % 2 != 0 && !evenNums) {
+                System.out.println(secondNums.get(i));
+            }
+        }
+
+        scanner.close();
+    }
 }
--- src/Es3/Main.java
+++ src/Es3/Main.java
@@ -1,4 +1,18 @@
 package Es3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Map<String, String> contacts = new HashMap<>();
+
+        addContact(contacts, "Rupert", "483275011");
+        System.out.println(contacts);
+    }
+
+    public static void addContact(Map<String, String> contacts, String name, String number) {
+        contacts.put(name, number);
+    }
 }
+
